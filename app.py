42c8774eeffe7af7d5aa02f92a42715536cc768f
josdic1 <joshuadicker@gmail.com>
2026-01-31 19:18:37 -0500

    add: Railway URL to CORS

--- app.py
+++ app.py
@@ -21,7 +21,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:8081",
+        "https://sterling-backend.up.railway.app",  # ADD THIS
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
